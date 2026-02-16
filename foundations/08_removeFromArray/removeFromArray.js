a8112dd05b216fea649c942c940212e66cea3674
Cameron Cruz <134180102+Cameron1Cruz@users.noreply.github.com>
2026-02-16 16:38:18 -0600

    Complete 08_removeFromArray.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    
+    // Loop through each item of the sourceArray and decide whether it shoiuld stay 
+    return array.filter(item => {
+
+        // Check if this item exists in the removal list
+        const shouldRemove = itemsToRemove.includes(item);
+
+        // Keep this item only if it is NOT in the removal list
+        return !shouldRemove;
+
+    })
 };
 
 // Do not edit below this line
